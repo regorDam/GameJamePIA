701be731f0950de1013728d855b3fc8436eeeb76
regorDam <regorm88@gmail.com>
2016-04-09 10:52:51 +0200

    SpawnManager

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnManager : MonoBehaviour 
+{
+
+	public GameObject playerPref;
+	public GameObject enemyPref;
+	public Transform spawn1, spawn2;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Spawn ();
+	
+	}
+	
+	void Spawn()
+	{
+		GameObject player = (GameObject)Instantiate (playerPref, spawn1.position, Quaternion.identity);
+
+		
+	}
+}
